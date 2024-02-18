961812f40f917e72133693b56c5f5edee476a0b3
oceane <oceaneverdi@protonmail.com>
2024-02-18 19:08:02 +0100

    first

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>dina
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>http://dina.ga</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="http://dina.ga/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="http://dina.ga/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://dina.ga/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="http://dina.ga/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="http://dina.ga/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
